811c758cec781701eca44c90e5d73d1abc4b6356
Андрей Мамаев <street.len@gmail.com>
2022-08-03 14:37:11 +0300

    change host

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -83,7 +83,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-INTERNAL_IPS = ['127.0.0.1', 'localhost', '158.160.6.6']
+INTERNAL_IPS = ['127.0.0.1', 'localhost', '158.160.6.241']
 
 AUTH_USER_MODEL = 'users.User'
 
